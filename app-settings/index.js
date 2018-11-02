63f29f6cec8a6c14b0d693b7e5cf6a096c5ad925
helabenkhalfallah <hela@upperskills.com>
2018-11-02 15:17:10 +0100

    [news_kit_frontend] adding localizations

--- app-settings/index.js
+++ app-settings/index.js
@@ -3,11 +3,13 @@ import BodyProvider from "./body/BodyProvider";
 import LanguageManager from "./language/LanguageManager";
 import ThemeManager from "./theme/ThemeManager";
 import LocalizedStringUtils from "./localized-strings/LocalizedStringUtils";
+import Localizations from "./localized-strings/Localizations";
 import Dico from "./localized-strings/Dico";
 
 const AppSettings = {
   LanguageManager,
   LocalizedStringUtils,
+  Localizations,
   Dico,
   ThemeManager,
   BodyProvider,
