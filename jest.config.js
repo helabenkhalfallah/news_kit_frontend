a903aec0130efed4567b68ca424f19a87c2c11d3
helabenkhalfallah <hela@upperskills.com>
2018-11-13 15:05:38 +0100

    [news_kit_frontend] add circleci reports folder

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   setupFiles: ['<rootDir>/jest.setup.js'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  reporters: [
+    "default",
+    [
+      "jest-junit", {
+        "outputDirectory": "./app-ci-reports",
+        "outputName": "./jest/results.xml",
+      }
+    ]
+  ]
 }
