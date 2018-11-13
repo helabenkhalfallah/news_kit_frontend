5a429d61c3be143ed7054b76d9fa1da910c71e76
helabenkhalfallah <hela@upperskills.com>
2018-11-13 09:10:42 +0100

    [news_kit_frontend] refactor

--- app-services/index.js
+++ app-services/index.js
@@ -1,18 +1,18 @@
-import USERS_LIST from "./graphql/remote-state/queries/USERS_LIST";
-import USER_SIGN_IN from "./graphql/remote-state/mutations/USER_SIGN_IN";
-import USER_IS_VALID from "./graphql/remote-state/queries/USER_IS_VALID";
+import USERS_LIST from "./remote-services/queries/USERS_LIST";
+import USER_SIGN_IN from "./remote-services/mutations/USER_SIGN_IN";
+import USER_IS_VALID from "./remote-services/queries/USER_IS_VALID";
 import {
   SIDE_BAR_STATUS,
   SIDE_BAR_OPEN,
   SIDE_BAR_CLOSE,
   SIDE_BAR_TOGGLE
-} from "./graphql/local-state/queries-mutations/SIDE_BAR_QM";
+} from "./local-services/queries-mutations/SIDE_BAR_QM";
 
 // import local state resolvers and defaults
 import {
   SIDE_BAR_DEFAULTS,
   SIDE_BAR_RESOLVERS
-} from "./graphql/local-state/resolvers-defaults/SIDE_BAR_RD";
+} from "./local-services/resolvers-defaults/SIDE_BAR_RD";
 
 // eslint-disable-next-line prettier/prettier
 export {
